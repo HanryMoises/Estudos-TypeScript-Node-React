930f564260dba83abb1d40ee116db375385c5c07
HanryMoises <oestudantetop@gmail.com>
2023-01-22 07:11:53 -0300

    js modules

---
+++ js/Class.js
@@ -0,0 +1,16 @@
+export class Soma{
+    obj = {
+        soma(a, b) {
+            return console.log(a + b)
+        },
+    }
+
+}
+
+export  class Multiplicao{
+    obj = {
+        multiplicacao(a, b) {
+            return console.log(a * b)
+        },
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import {Soma,Multiplicao} from "./Class.js"
+const a = new Soma ();
+const b = new Multiplicao ();
+
+a.obj.soma(2,2)
+b.obj.multiplicacao(2,3)
